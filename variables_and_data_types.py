21862388713809be98c04a245b9c37149a939ade
Omkar <omkaramate1234@gmail.com>
2023-12-08 18:03:51 +0530

    add variables and data type examples

---
+++ variables_and_data_types.py
@@ -0,0 +1,20 @@
+age = 30 # Integer
+price = 10.2 # Float
+full_name = "Omkar Amate" # string
+is_happy = False # Boolean
+
+print("Age is ", age)
+print("Rupees" , price);
+
+price = 30.5
+print("Rupees" , price);
+
+print("Name is ", full_name.upper())
+
+# variables
+# values For eg- value of variable age is 30
+# data types - integer , float, string, boolean. For eg - data type of variable age is integer
+
+#variable name is case sensitive . Vairable names should always start with lower case. If more than one words are present in variable name then seperate them with  '_'. For example - full_name, account_number
+
+# Operations are data type specific. For eg - upper() can be only performed on strings and not on integers or floats
